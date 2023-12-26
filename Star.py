17ac8bfa253d74049a92436a4483565990378d78
Kanber Buldu <kanberbuldu@gmail.com>
2023-12-26 15:11:34 +0300

    add star

---
+++ Star.py
@@ -0,0 +1,24 @@
+import turtle
+
+
+draw_screen = turtle.Screen()
+draw_screen.bgcolor("light blue")
+
+turtle_instance = turtle.Turtle()
+
+for i in range(5)
+
+    turtle_instance.right(144)
+    turtle_instance.forward(200)
+
+
+
+
+
+
+
+
+turtle.done()
+
+
+
