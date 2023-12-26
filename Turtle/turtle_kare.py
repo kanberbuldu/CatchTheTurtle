b3a5b4c0a22bee8e94a5fa6d5a76074257e70732
Kanber Buldu <kanberbuldu@gmail.com>
2023-12-26 11:24:28 +0300

    change main to turtle_kare

---
+++ Turtle/turtle_kare.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+draw_screen = turtle.Screen()
+draw_screen.bgcolor("light blue")
+draw_screen.title("Kare Çizimi")
+
+instance_turtle = turtle.Turtle()
+
+for i in range(4):
+    instance_turtle.left(90)
+    instance_turtle.forward(200)
+
+
+
+
+
+turtle.done()
+
+
